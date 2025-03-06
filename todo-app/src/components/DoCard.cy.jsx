cdfeec6c495183f227a2f72324ae731432153bda
Kacper Frantczak <kfrantczakk@gmail.com>
2025-03-06 22:24:45 +0100

    6

--- todo-app/src/components/DoCard.cy.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { DoCard } from './DoCard'
-
-describe('<DoCard />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<DoCard />)
-  })
-})
